3532434b31ea93ddd4da278c978861a725f2dc72
Bernardo Toledo <btoledo586@gmail.com>
2025-09-30 21:39:33 -0300

    Adicionado a tela de Login ao projeto, realizada a estilização básica da tela.

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ProductDetailScreen from './screens/ProductDetailScreen';
 import { COLORS } from './constants/colors';
+
+import LoginScreen from './screens/LoginScreen';
+import ProductDetailScreen from './screens/ProductDetailScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ProductDetailScreen />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="light" backgroundColor={COLORS.background} />
     </SafeAreaProvider>
   );
